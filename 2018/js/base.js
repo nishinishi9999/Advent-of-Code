d164939be8510ffd1b462cf94bc0801ab4d84e39
nishinishi <nishinishi9999@protonmail.com>
2018-12-01 16:25:29 +0100

    cleanup

--- 2018/js/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const fs = require('fs');
-const ramda = require('ramda');
-function read_file(path) {
-    return fs.readFileSync(path, 'utf8');
-}
-function main() {
-    const input = read_file('../input/day_1.txt');
-}
